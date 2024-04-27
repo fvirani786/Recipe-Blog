6ac29c03a96a102604a7cc21714bdbbd7893ce11
tflinch <tflinch16@gmail.com>
2024-04-27 13:17:47 -0500

    feat: added typed.js to fruits,meats,veggies index.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,35 @@
 /*==================== typed js ====================*/
-const typed = new Typed(".multiple-text", {
-  strings: ["Frontend Engineer", "Backend Engineer", "FullStack Engineer"],
+const typed = new Typed(".title-fruit", {
+  strings: ["Frutas", "フルーツ", "ፍሬ"],
+  startDelay: 1000,
   typeSpeed: 100,
   backSpeed: 100,
   backDelay: 1000,
   loop: true,
+});
+
+const mTyped = new Typed(".title-meats", {
+  strings: ["Carnes", "肉", "ስጋዎች"],
+  startDelay: 1000,
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
+
+const vTyped = new Typed(".title-veggies", {
+  strings: ["Verduras", "野菜", "አትክልቶች"],
+  startDelay: 1000,
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
+
+/*==================== scroll reveal ====================*/
+ScrollReveal({
+  reset: true,
+  distance: "80px",
+  duration: 2000,
+  delay: 200,
 });
