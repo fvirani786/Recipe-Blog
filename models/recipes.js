1a9bba0e8e0ea357731c8b8b3d0b9c23ad609f2f
tflinch <tflinch16@gmail.com>
2024-04-30 15:03:58 -0400

    feat: add new,delete,edit ejs

---
+++ models/recipes.js
@@ -0,0 +1,31 @@
+const recipes = [
+  {
+    dish: "Stry Fry",
+    userName: "FoodStar",
+    likes: 10,
+    img: "https://imgs.search.brave.com/CQ3HfT5V2dJnU51nPpShICULHw8XMErOpMVCQZLBBVM/rs:fit:500:0:0/g:ce/aHR0cHM6Ly93d3cu/bW9tb250aW1lb3V0/LmNvbS93cC1jb250/ZW50L3VwbG9hZHMv/MjAxOC8xMS9zdGly/LWZyeS1jaGlja2Vu/LTczM3gxMTAyLmpw/Zw",
+  },
+  {
+    dish: "Jamaican Curry",
+    userName: "The Only Eater",
+    likes: 5,
+    img: "https://imgs.search.brave.com/iihVfqkd5hFXSZe79XxN3v8IFRMGh5Vb8wJVGBjUfhY/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9pMC53/cC5jb20vamFtYWlj/YWNhZmUub3JnL3dw/LWNvbnRlbnQvdXBs/b2Fkcy8yMDIxLzA3/L2ltZ180Njg3LTEt/c2NhbGVkLmpwZz9m/aXQ9ODAwLDEwNjcm/c3NsPTE",
+  },
+  {
+    dish: "Bruschetta ",
+    userName: "Bruncher9000",
+    likes: 8,
+    img: "https://imgs.search.brave.com/MbOeCK19-oLuHydjwmhxpark28DyCUUKk4M8tpHqwA4/rs:fit:500:0:0/g:ce/aHR0cHM6Ly93d3cu/c2ltcGx5cmVjaXBl/cy5jb20vdGhtYi8x/MjdEWFpXejVOZGt4/RlE5ZDFuM3NjS0Q0/dU09LzE1MDB4MC9m/aWx0ZXJzOm5vX3Vw/c2NhbGUoKTptYXhf/Ynl0ZXMoMTUwMDAw/KTpzdHJpcF9pY2Mo/KS9TaW1wbHktUmVj/aXBlcy1CcnVzY2hl/dHRhLVRvbWF0by1C/YXNpbC1MRUFELTMt/NzcyZmQxMWRlNDE0/NDU1MmE0ODVhZjg3/ZjcwMzNiZjguanBn",
+  },
+  {
+    dish: "Ethiopian Tibs",
+    userName: "Anna Banana",
+    likes: 10,
+    img: "https://imgs.search.brave.com/GRlDkPjYtCkiQEkHsU9w7BlGUzjlHk0DxUNhfIgddQM/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9jb29r/c21hcnRzLmltZ2l4/Lm5ldC9tZWFsX3Bo/b3Rvcy8yMDY5LzIw/MjIxMDE3X0JlZWZf/VGlicy1OTS0xLmpw/Zz9peGxpYj1yYWls/cy00LjMuMCZ3PTUw/MCZkcHI9MiZhdXRv/PWZvcm1hdA",
+  },
+];
+
+module.exports = {
+  recipes,
+};
+// i can now import this into another file
--- server.js
+++ server.js
@@ -1,20 +1,30 @@
 const express = require("express");
 const app = express(); // our app
 const PORT = process.env.PORT || 3000;
+// middleware for PUT and DELETE Methods
+const methodOverride = require("method-override");
 
 // ------------ DATA -----------------
 // inside of fruits.js
 const { fruits } = require("./models/fruits");
 const { veggies } = require("./models/veggies");
 const { meats } = require("./models/meats");
+const { recipes } = require("./models/recipes");
 
 // ------------ MIDDLEWARE ------------
+app.use(methodOverride("_method"));
 app.set("view engine", "ejs"); // come back to this
 app.use("/", express.static("public"));
 // app.use(express.static(__dirname + "/public/"));
+//allows use to parse data through and see the data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // ------------ ROUTES ---------------
 // ******* INDEX ROUTE **********
+app.get("/", (req, res) => {
+  res.render("home/index");
+});
 app.get("/fruits", (req, res) => {
   // send array as a response
   res.render("fruits/index", {
@@ -22,6 +32,10 @@ app.get("/fruits", (req, res) => {
     allVeggies: veggies,
     allMeats: meats,
   });
+});
+
+app.get("/fruits/new", (req, res) => {
+  res.render("fruits/new.ejs", {});
 });
 app.get("/veggies", (req, res) => {
   // send array as a response
@@ -40,6 +54,12 @@ app.get("/meats", (req, res) => {
   });
 });
 
+app.get("/recipes", (req, res) => {
+  res.render("recipe/index", {
+    allRecipes: recipes,
+  });
+});
+
 // ******* SHOW ROUTE **********
 app.get("/fruits/:indexOfFruitsArray", (req, res) => {
   let idx = parseInt(req.params.indexOfFruitsArray);
@@ -49,8 +69,20 @@ app.get("/fruits/:indexOfFruitsArray", (req, res) => {
     res.render("404", {});
   } else {
     // res.send(fruits[idx]);
-    res.render("fruits/show", { fruit: fruits[idx] });
+    res.render("fruits/show", { fruit: fruits[idx], id: idx });
   }
+});
+//********* GET -edit page get rout
+app.get("/fruits/:id/edit", (req, res) => {
+  const fruit = fruits[req.params.id];
+  let id = parseInt(req.params.id);
+  res.render("fruits/edit", { fruit: fruit, id: id });
+});
+// *****GET -DELETE PAGE**********
+app.get("/fruits/:id/delete", (req, res) => {
+  const fruit = fruits[req.params.id];
+  let id = parseInt(req.params.id);
+  res.render("fruits/delete", { fruit, id });
 });
 app.get("/veggies/:indexOfVeggiesArray", (req, res) => {
   let idx = parseInt(req.params.indexOfVeggiesArray);
@@ -73,6 +105,37 @@ app.get("/meats/:indexOfMeatsArray", (req, res) => {
     // res.send(fruits[idx]);
     res.render("meats/show", { meat: meats[idx] });
   }
+});
+// ***** POST NEW FRUIT ******
+app.post("/fruits", (req, res) => {
+  console.log("-------- FORM BODY \n", req.body);
+  if (req.body.readyToEat === "on") {
+    req.body.readyToEat = true;
+  } else {
+    req.body.readyToEat = false;
+  }
+
+  fruits.push(req.body);
+  res.redirect("/fruits");
+});
+
+// ****** update fruit ***
+app.put("/fruits/:id", (req, res) => {
+  console.log("-----Update Fruit--------- \n", req.body);
+  if (req.body.readyToEat === "on") {
+    req.body.readyToEat = true;
+  } else {
+    req.body.readyToEat = false;
+  }
+  fruits[parseInt(req.params.id)] = req.body;
+  res.redirect("/fruits");
+});
+
+//***DELETE- DELETE FRUIT */
+app.delete("/fruits/:id", (req, res) => {
+  // remove the fruit item from the fruits array
+  fruits.splice(parseInt(req.params.id), 1);
+  res.redirect("/fruits");
 });
 
 // ----------- LISTEN FOR SERVER ----------
